58d7015adfa9dab4c46ba1e61a222b6390e01eee
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-06-24 15:16:31 -0400

    started setting up user authentication

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,8 +1,12 @@
 import "./AuthPage.css"
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
 
 export default function AuthPage() {
   return (
+    <main>
       <h1>AuthPage</h1>
+      <SignUpForm/>
+    </main>
   );
 }
 
