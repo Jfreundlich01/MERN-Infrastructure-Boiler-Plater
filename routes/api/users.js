17aa719447ab994339efb5672b2ed9f4d25795cc
Jfreundlich01 <jordan.freundlich@gmail.com>
2022-06-29 09:28:03 -0400

    added ensureloggedin

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,15 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
+// require the authorization middleware function
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 // POST /api/users
 router.post('/', usersCtrl.create);
 
 router.post('/login', usersCtrl.login)
 
-router.get('/check-token', usersCtrl.checkToken);
+router.get('/check-token', ensureLoggedIn,usersCtrl.checkToken);
+
 
 module.exports = router;
